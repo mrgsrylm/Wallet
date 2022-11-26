43e92885b1ec8c9d823bd21b7868011913ba1419
Agus S. Mubarok <gusrylmubarok@gmail.com>
2022-11-26 13:39:07 +0700

    creat UI authentication

--- social-media/src/App.js
+++ social-media/src/App.js
@@ -1,23 +1,82 @@
-import logo from './logo.svg';
-import './App.css';
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Outlet,
+  Navigate
+} from 'react-router-dom';
+
+import NavBar from './components/navBar/NavBar';
+import LeftBar from './components/leftBar/LeftBar';
+import RightBar from './components/rightBar/RightBar';
+import Login from './pages/login/Login';
+import Register from './pages/register/Register';
+import Home from './pages/home/Home';
+import Profile from './pages/profile/Profile';
+
 
 function App() {
+
+  const currentUser = false;
+
+  /*
+  ** Layout For Home Page Social Media
+  */
+  const Layout = () => {
+    return(
+      <div>
+        <NavBar />
+        <div style={{display: "flex"}}>
+          <LeftBar />
+          <Outlet />
+          <RightBar />
+        </div>
+      </div>
+    )
+  }
+
+  const ProtectedRoute = ({children}) => {
+    if(!currentUser) {
+      return <Navigate to="/login" />
+    }
+    return children
+  }
+
+  /*
+  ** Router for switch between pages on Social Media
+  */
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: (
+        <ProtectedRoute>
+          <Layout />
+        </ProtectedRoute>
+        ),
+      children: [
+        {
+          path: "/",
+          element: <Home />
+        },
+        {
+          path: "/profile/:id",
+          element: <Profile />
+        }
+      ]
+    },
+
+    {
+      path: "/login",
+      element: <Login />
+    },
+    {
+      path: "/register",
+      element: <Register />
+    },
+  ]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <RouterProvider router={router} />
     </div>
   );
 }
---
+++ social-media/src/pages/register/Register.jsx
@@ -0,0 +1,32 @@
+import { Link } from 'react-router-dom';
+
+import './register.scss';
+
+const Register = () => {
+  return (
+    <div className="register">
+      <div className="card">
+        <div className="left">
+          <h1>Social Media</h1>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio, rerum quidem distinctio inventore ad magnam nemo beatae! Ad, eos delectus.</p>
+          <span>Do you have an account?</span>
+          <Link to="/login">
+            <button>Login</button>
+          </Link>
+        </div>
+        <div className="right">
+          <h1>Register</h1>
+          <form>
+            <input type="text" placeholder="Full Name" />
+            <input type="text" placeholder="Username" />
+            <input type="text" placeholder="Email" />
+            <input type="password" placeholder="Password" />
+            <button>Register</button>
+          </form>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Register
